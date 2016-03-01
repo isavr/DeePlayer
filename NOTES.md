db9c4b84543a1794bb7802e39eedcb74e839b726
ilya.savritsky <ilya.savritsky@hiqo-solutions.com>
2016-03-01 14:33:03 +0300

    - NOTES updated - Some cleanup

--- NOTES.md
+++ NOTES.md
@@ -2,6 +2,8 @@ What's next:
 - Add filtration to all views
 - Remove useless layouts
 - Fix progress dialog dismission on process finish
+- Fix favourite tracks list update for Music service
+- Fix main item selection (NO USAGE OF STRING KEYS!)
 - Rethink main layout
 - Consider: add player layout to the app
 - Add recommended playlists & user playlists views
--- app/src/main/java/com/tutorial/deeplayer/app/deeplayer/adapters/MainViewAdapter.java
+++ app/src/main/java/com/tutorial/deeplayer/app/deeplayer/adapters/MainViewAdapter.java
@@ -17,26 +17,23 @@ import butterknife.ButterKnife;
  * Created by ilya.savritsky on 15.09.2015.
  */
 public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.MenuDataHolder> {
+    private static final int DEFAULT_SELECTED_ITEM = -1; // no selection
     private String[] menuItems;
     private Context context;
     private ViewHolderClicks listener;
-    private static final int DEFAULT_SELECTED_ITEM = -1; // no selection
     private int selectedItemIndex;
 
     public MainViewAdapter(Context context, String[] items, ViewHolderClicks listener) {
         super();
         this.menuItems = items;
         this.context = context;
-        this.selectedItemIndex = -1;
+        this.selectedItemIndex = DEFAULT_SELECTED_ITEM;
         this.listener = listener;
     }
 
     @Override
     public MenuDataHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(context).inflate(R.layout.main_view_item, viewGroup, false);
-//        final MenuDataHolder menuHolder = new MenuDataHolder(view, listener);
-//        final String text = menuItems[i];
-//        menuHolder.textView.setText(text);
         return new MenuDataHolder(view, listener);
     }
 
@@ -44,15 +41,6 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.MenuDa
     public void onBindViewHolder(MenuDataHolder menuDataHolder, int i) {
         final String text = menuItems[i];
         menuDataHolder.position = i;
-//        if (i == selectedItemIndex) {
-//            menuDataHolder.textView.setSelected(true);
-//            menuDataHolder.cardView.setCardElevation(5);
-//            menuDataHolder.cardView.setCardBackgroundColor(R.color.primary_green);
-//        } else {
-//            menuDataHolder.textView.setSelected(false);
-//            menuDataHolder.cardView.setCardElevation(0);
-//            menuDataHolder.cardView.setCardBackgroundColor(R.color.primary_green_dark);
-//        }
         menuDataHolder.textView.setChecked(i == selectedItemIndex);
         menuDataHolder.textView.setText(text);
     }
@@ -63,6 +51,20 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.MenuDa
             return 0;
         }
         return menuItems.length;
+    }
+
+    public void selectItemAtPos(int position) {
+//        int previousIndex = selectedItemIndex;
+        this.selectedItemIndex = position;
+        notifyDataSetChanged();
+//        if (previousIndex != -1) {
+//            notifyItemChanged(previousIndex);
+//        }
+//        notifyItemChanged(position);
+    }
+
+    public interface ViewHolderClicks {
+        void onItemClick(String key);
     }
 
     public static class MenuDataHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -90,19 +92,5 @@ public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.MenuDa
                 listener.onItemClick(key);
             }
         }
-    }
-
-    public void selectItemAtPos(int position) {
-//        int previousIndex = selectedItemIndex;
-        this.selectedItemIndex = position;
-        notifyDataSetChanged();
-//        if (previousIndex != -1) {
-//            notifyItemChanged(previousIndex);
-//        }
-//        notifyItemChanged(position);
-    }
-
-    public interface ViewHolderClicks {
-        void onItemClick(String key);
     }
 }
